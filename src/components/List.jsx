eb0dba455dc52d4ea0f83e3b52546f069ee08277
kibo13 <kimboris1310@gmail.com>
2022-03-24 23:39:27 +0600

    feat: added data list virtualization

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,25 +1,42 @@
-function List(props) {
-    return (
-        <ul className="apps-list">
-            {props.apps.map((app) => {
-                return (
-                    <li className="apps-list-item" key={app.id}>
-                        <p className="my-5 text-uppercase text-underline">
-                            {props.clients.find((client) => client.id === app.client_id).name}
-                        </p>
-                        <p>
-                            <span className="text-bold">Тип заявки: </span>
-                            {app.type === 'delivery' ? ' доставка' : ' забор'}
-                        </p>
-                        <p>
-                            <span className="text-bold">Цена: </span>
-                            {app.price}
-                        </p>
-                    </li>
-                )
-            })}
-        </ul>
-    )
-}
+import { FixedSizeList } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
+
+const List = ({ apps, clients }) => (
+    <AutoSizer>
+        {({ height, width }) => (
+            <FixedSizeList
+                innerElementType={'ul'}
+                className="apps-list"
+                height={height}
+                width={width}
+                itemSize={80}
+                itemCount={apps.length}
+            >
+                {({ index, style }) => {
+                    return (
+                        <li
+                            style={style}
+                            className={
+                                index % 2 ? 'apps-list-item bg-odd' : 'apps-list-item bg-even'
+                            }
+                        >
+                            <p className="my-5 text-uppercase text-underline">
+                                {clients.find((client) => client.id === apps[index].client_id).name}
+                            </p>
+                            <p>
+                                <span className="text-bold">Тип заявки: </span>
+                                {apps[index].type === 'delivery' ? ' доставка' : ' забор'}
+                            </p>
+                            <p>
+                                <span className="text-bold">Цена: </span>
+                                {apps[index].price}
+                            </p>
+                        </li>
+                    )
+                }}
+            </FixedSizeList>
+        )}
+    </AutoSizer>
+)
 
 export default List
